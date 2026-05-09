0c90a9d893a8cb6759aecfd2aebf5c42526c97b3
orvinjedalava <ojedbub@gmail.com>
2026-05-09 22:32:20 +1000

    Complete #10

---
+++ complete-cpp-developer-course-2025-main/main/Section-002/SourceFiles/booleanFun.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+  bool isRaining = false;
+  std::cout << std::boolalpha; // print bool values as true/false instead of 1/0
+
+  std::cout << isRaining << std::endl;
+
+  return 0;
+}
