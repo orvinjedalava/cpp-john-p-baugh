fd2ade9fb8bc1478a73d84f80fe9a7e6bfae6035
orvinjedalava <ojedbub@gmail.com>
2026-05-09 22:25:40 +1000

    Complete #9

---
+++ complete-cpp-developer-course-2025-main/main/Section-002/SourceFiles/textFun.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+  char singleChar = 'a';
+  std::string myName = "John Doe";
+
+  std::cout << singleChar << std::endl;
+  std::cout << myName << std::endl;
+
+  std::cout << "Hello " << myName << std::endl;
+
+  return 0;
+}
